d20f5c49c3908c838919911a7de1fe045a365ecf
Angel87-dev <123046088@upq.edu.mx>
2026-05-11 16:21:17 -0600

    1.3 La cámara de las reglas

--- Survival Guide.py
+++ Survival Guide.py
@@ -3,77 +3,98 @@ print("A continuación se mostraran los 4 nieveles que debes pasar")
 
 print("============")
 correctas = 0
-print("La Cámara de las Reglas: Donde se explican las normas de convivencia y asistencia.")
-print("Conforme a las siguientes opciones...")
-print("1. Respeto, 2. Falta de compromiso, 3. Ser grosero")
-respuesta = input("¿Que se tiene que mostrar en clase?: ")
-if respuesta == "1":
-    print("Respuesta correcta")
-    correctas += 1
-    print("Llevas: " ,correctas, "respuestas correctas")
-else:
-    print("Respuesta equivocada")
-    print("Llevas: " ,correctas, "respuestas correctas")
+while True:
+    print("La Cámara de las Reglas: Donde se explican las normas de convivencia y asistencia.")
+    print("Conforme a las siguientes opciones...")
+    print("1. Respeto, 2. Falta de compromiso, 3. Ser grosero")
+    respuesta = input("¿Que se tiene que mostrar en clase?: ")
+    if respuesta == "1":
+        print("Respuesta correcta")
+        correctas += 1
+        print("Llevas: " ,correctas, "respuestas correctas")
+    else:
+        print("Respuesta equivocada")
+        print("Llevas: " ,correctas, "respuestas correctas")
 
-print("")
-print("Es _______ particiar en clase")
-print("1. Irrelevante, 2. Importante, 3. Considerable")
-respuesta = input("¿Cual es tu respuesta?: ")
-if respuesta == "2":
-    print("Respuesta correcta")
-    correctas += 1
-    print("Llevas: " ,correctas, "respuestas correctas")
-else:
-    print("Respuesta equivocada")
-    print("Llevas: " ,correctas, "respuestas correctas")
+    print("")
+    print("Es _______ particiar en clase")
+    print("1. Irrelevante, 2. Importante, 3. Considerable")
+    respuesta = input("¿Cual es tu respuesta?: ")
+    if respuesta == "2":
+        print("Respuesta correcta")
+        correctas += 1
+        print("Llevas: " ,correctas, "respuestas correctas")
+        if correctas >= 2:
+            decision = input("Checkpoint desbloqueado ¿Quieres pasar al siguiente nivel? (1=Si/0=No): ")
+            if decision == "1":
+                break
+    else:
+        print("Respuesta equivocada")
+        print("Llevas: " ,correctas, "respuestas correctas")
 
-print("")
-print("¿A las cuantas faltas te vas a final del parcial?")
-print("1. Dos faltas, 2. Tres faltas, 3. Cinco faltas")
-respuesta = input("¿Cuál es tu respuesta?: ")
-if respuesta == "2":
-    print("Respuesta correcta")
-    correctas += 1
-    print("Llevas: " ,correctas, "respuestas correctas")
-else:
-    print("Respuesta equivocada")
-    print("Llevas: " ,correctas, "respuestas correctas")
+    print("")
+    print("¿A las cuantas faltas te vas a final del parcial?")
+    print("1. Dos faltas, 2. Tres faltas, 3. Cinco faltas")
+    respuesta = input("¿Cuál es tu respuesta?: ")
+    if respuesta == "2":
+        print("Respuesta correcta")
+        correctas += 1
+        print("Llevas: " ,correctas, "respuestas correctas")
+        if correctas >= 2:
+            decision = input("¿Quieres pasar al siguiente nivel? (1=Si/0=No): ")
+            if decision == "1":
+                break
+    else:
+        print("Respuesta equivocada")
+        print("Llevas: " ,correctas, "respuestas correctas")
 
-print("")
-print("¿Cuál es la calificación máxima en un final?")
-print("1. Diez, 2. Nueve, 3. Ocho, 4. Siete")
-respuesta = input("¿Cuál es tu respuesta?: ")
-if respuesta == "3":
-    print("Respuesta correcta")
-    correctas += 1
-    print("Llevas: " ,correctas, "respuestas correctas")
-else:
-    print("Respuesta equivocada")
-    print("Llevas: " ,correctas, "respuestas correctas")
+    print("")
+    print("¿Cuál es la calificación máxima en un final?")
+    print("1. Diez, 2. Nueve, 3. Ocho, 4. Siete")
+    respuesta = input("¿Cuál es tu respuesta?: ")
+    if respuesta == "3":
+        print("Respuesta correcta")
+        correctas += 1
+        print("Llevas: " ,correctas, "respuestas correctas")
+        if correctas >= 2:
+            decision = input("¿Quieres pasar al siguiente nivel? (1=Si/0=No): ")
+            if decision == "1":
+                break
+    else:
+        print("Respuesta equivocada")
+        print("Llevas: " ,correctas, "respuestas correctas")
 
-print("")
-print("¿Qué pasa si se encuentra plagio en los trabajos?")
-print("1. Cero para todos, 2. Se puede volver a hacer el trabajo, 3. Cero solamente para la persona que copio")
-respuesta = input("¿Cuál es tu respuesta?: ")
-if respuesta == "1":
-    print("Respuesta correcta")
-    correctas += 1
-    print("Llevas: " ,correctas, "respuestas correctas")
-else:
-    print("Respuesta equivocada")
-    print("Llevas: " ,correctas, "respuestas correctas")
+    print("")
+    print("¿Qué pasa si se encuentra plagio en los trabajos?")
+    print("1. Cero para todos, 2. Se puede volver a hacer el trabajo, 3. Cero solamente para la persona que copio")
+    respuesta = input("¿Cuál es tu respuesta?: ")
+    if respuesta == "1":
+        print("Respuesta correcta")
+        correctas += 1
+        print("Llevas: " ,correctas, "respuestas correctas")
+        if correctas >= 2:
+            decision = input("¿Quieres pasar al siguiente nivel? (1=Si/0=No): ")
+            if decision == "1":
+                break
+    else:
+        print("Respuesta equivocada")
+        print("Llevas: " ,correctas, "respuestas correctas")
 
-print("")
-print("¿Como se tienen que entregar los trabajos?")
-print("1. Incompletos, 2. Minimo con la mitad de lo que se requiere, 3. Completos")
-respuesta = input("¿Cuál es tu respuesta?: ")
-if respuesta == "3":
-    print("Respuesta correcta")
-    correctas += 1
-    print("Llevas: " ,correctas, "respuestas correctas")
-else:
-    print("Respuesta equivocada")
-    print("Llevas: " ,correctas, "respuestas correctas")
+    print("")
+    print("¿Como se tienen que entregar los trabajos?")
+    print("1. Incompletos, 2. Minimo con la mitad de lo que se requiere, 3. Completos")
+    respuesta = input("¿Cuál es tu respuesta?: ")
+    if respuesta == "3":
+        print("Respuesta correcta")
+        correctas += 1
+        print("Llevas: " ,correctas, "respuestas correctas")
+        if correctas >= 2:
+            decision = input("¿Quieres pasar al siguiente nivel? (1=Si/0=No): ")
+            if decision == "1":
+                break
+    else:
+        print("Respuesta equivocada")
+        print("Llevas: " ,correctas, "respuestas correctas")
 
 print("============")
 print("El Oráculo de las Notas: Donde se detallan los porcentajes de evaluación.")
